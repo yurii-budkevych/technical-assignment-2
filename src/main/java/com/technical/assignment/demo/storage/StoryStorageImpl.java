9e40b9efee3ecbe5c9bed384d81072792f64fe29
yurii-budkevych <60780508+yurii-budkevych@users.noreply.github.com>
2020-02-13 10:07:04 +0100

    Add offline mode. Separate DTO-Entity

--- src/main/java/com/technical/assignment/demo/storage/StoryStorageImpl.java
+++ src/main/java/com/technical/assignment/demo/storage/StoryStorageImpl.java
@@ -25,21 +25,29 @@ public class StoryStorageImpl implements StoryStorage {
 
     SessionFactory sessionFactory;
 
+    private boolean offlineMode = false;
+
     public StoryStorageImpl(@Autowired SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
 
-    AbstractLinkedMap<String, Story> map = new LinkedMap<>();
+    AbstractLinkedMap<String, StoryEntity> map = new LinkedMap<>();
 
     @Override
     public void saveStories(List<Story> stories) {
-
+        if (stories == null || stories.size() == 0) {
+            offlineMode = true;
+            return;
+        }
+        System.out.println(stories.get(0).getId());
+        offlineMode = false;
         for (Story story : stories) {
             if (map.containsKey(story.getTitle())) continue;
             if (map.size() >= STORAGE_CAPACITY) {
                 map.remove(map.lastKey());
+                sessionFactory.getCurrentSession().delete(map.get(map.lastKey()));
             }
-            map.put(story.getTitle(), story);
+            map.put(story.getTitle(), new StoryEntity(story.getId(), story.getTitle(), story.getTimeAgo(), story.getUrl()));
         }
 
         persist();
@@ -51,14 +59,18 @@ public class StoryStorageImpl implements StoryStorage {
 
     @Override
     public List<Story> getStories() {
-        CriteriaBuilder builder = sessionFactory.getCurrentSession().getCriteriaBuilder();
-        CriteriaQuery<StoryEntity> criteria = builder.createQuery(StoryEntity.class);
-        criteria.from(StoryEntity.class);
-        List<StoryEntity> data = sessionFactory.getCurrentSession().createQuery(criteria).getResultList();
-
         List<Story> result = new ArrayList<>();
-        for (StoryEntity storyEntity : data) {
-            result.add(new Story(storyEntity.getTitle(), storyEntity.getTimeAgo(), storyEntity.getUrl()));
+        if (offlineMode) {
+            CriteriaBuilder builder = sessionFactory.getCurrentSession().getCriteriaBuilder();
+            CriteriaQuery<StoryEntity> criteria = builder.createQuery(StoryEntity.class);
+            criteria.from(StoryEntity.class);
+            List<StoryEntity> data = sessionFactory.getCurrentSession().createQuery(criteria).getResultList();
+
+            for (StoryEntity storyEntity : data) {
+                result.add(new Story(storyEntity.getTitle(), storyEntity.getTimeAgo(), storyEntity.getUrl()));
+            }
+        } else {
+            map.values().forEach(e -> result.add(new Story(e.getId(), e.getTitle(), e.getTimeAgo(), e.getUrl())));
         }
         return result;
     }
